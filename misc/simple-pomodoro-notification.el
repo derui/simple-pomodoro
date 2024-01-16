e0b3198c192406d2012e5dc4a8278df4c501a474
derui <derutakayu@gmail.com>
2024-01-17 07:53:18 +0900

    feat: add custom variable for icon

--- misc/simple-pomodoro-notification.el
+++ misc/simple-pomodoro-notification.el
@@ -23,6 +23,20 @@
 (require 'alert)
 (require 'simple-pomodoro)
 
+(defcustom simple-pomodoro-notification-icon nil
+  "Icon image path for notification. When this value is `nil', notification does not display any icon.
+User can set icon path for all state, or each state of pomodoro. If alist have a key `t', it is used as fallback icon.
+ Like configurations below.
+
+(custom-set-variable \='simple-pomodoro-notificatio-icon \"path of variable\") ;; set icon for all state
+(custom-set-variable \='simple-pomodoro-notificatio-icon \='((task . \"icon for task\") (fallback . \"fallback icon\"))) ;; set icon for each state.
+
+"
+  :type '(choice (alist :key-type symbol :value-type string
+                        :options (task task-finished short-break short-break-finished long-break long-break-finished stopped fallback))
+                 string)
+  :group 'simple-pomodoro)
+
 (defvar simple-pomodoro-notification--alert-style
   (cond
    ((eq system-type 'darwin) 'osx-notifier)
@@ -31,37 +45,51 @@
 
 ;; functions
 
+(defun simple-pomodoro-notification--get-icon (state)
+  "Get icon path for `state'"
+  (or (and (stringp simple-pomodoro-notification-icon) simple-pomodoro-notification-icon)
+      (alist-get state simple-pomodoro-notification-icon)
+      (alist-get 'fallback simple-pomodoro-notification-icon)))
+
 ;;;###autoload
 (defun simple-pomodoro-notification (kind)
   "Notify `KIND' to user."
-  (cl-case kind
-    (task
-     (alert "Task is started"
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (task-finished
-     (alert "Task is finished."
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (short-break
-     (alert "Short break is started."
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (short-break-finished
-     (alert "Short break is finished."
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (long-break
-     (alert "Long break is started."
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (long-break-finished
-     (alert "Long break is finished."
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))
-    (stopped
-     (alert "Timer is stopped"
-            :title "Simple Pomodoro"
-            :style simple-pomodoro-notification--alert-style))))
+  (let ((icon (simple-pomodoro-notification--get-icon kind)))
+    (cl-case kind
+      (task
+       (alert "Task is started"
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (task-finished
+       (alert "Task is finished."
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (short-break
+       (alert "Short break is started."
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (short-break-finished
+       (alert "Short break is finished."
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (long-break
+       (alert "Long break is started."
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (long-break-finished
+       (alert "Long break is finished."
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style))
+      (stopped
+       (alert "Timer is stopped"
+              :icon icon
+              :title "Simple Pomodoro"
+              :style simple-pomodoro-notification--alert-style)))))
 
 (provide 'simple-pomodoro-notification)
